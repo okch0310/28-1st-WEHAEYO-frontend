e8da81acd2779064ce1c496001db97238baa176b
minami-cs <mina9301@gmail.com>
2021-12-28 22:02:07 +0900

    Add: 상세페이지 컴포넌트 생성

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Signup from './pages/Signup/Signup';
 import Main from './pages/Main/Main';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
+import RestaurantDetail from './pages/RestaurantDetail/RestaurantDetail';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/restaurantdetail" element={<RestaurantDetail />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/pages/RestaurantDetail/RestaurantInfo.js
+++ src/pages/RestaurantDetail/RestaurantInfo.js
@@ -1,13 +1,29 @@
+import { ImStarEmpty, ImStarHalf, ImStarFull } from 'react-icons/im';
+import { MdPlace } from 'react-icons/md';
+import { BsTelephoneFill } from 'react-icons/bs';
+import './RestaurantDetail.scss';
+
 export default function RestaurantInfo() {
   return (
     <section>
-      <img alt="식당 이름" />
-      <div>
-        <p>식당 이름</p>
-        <p>별점</p>
-        <p>리뷰</p>
-        <p>주소</p>
-        <p>전화번호</p>
+      <img alt="맥도날드 선릉점" src="images/RestaurantDetail/macdonalds.png" />
+      <div className="restaurant_info">
+        <h1>맥도날드 선릉점</h1>
+        <p className="stars">
+          <ImStarFull />
+          <ImStarHalf />
+          <ImStarEmpty />
+          <ImStarEmpty />
+          <ImStarEmpty />
+          1.5점
+        </p>
+        <p>리뷰 15개</p>
+        <p>
+          <MdPlace /> 서울시 강남구 선릉1로 104
+        </p>
+        <p>
+          <BsTelephoneFill /> 02-172-2847
+        </p>
       </div>
     </section>
   );
