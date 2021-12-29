73f52416b31ae5eff22266fcda9de5b9f05dcde4
Young <81794430+Daeyeong-hub@users.noreply.github.com>
2021-12-29 18:12:34 +0900

    Modify: map과 component, mockdata 등을 활용해서 화면 구성

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Restaurant.scss';
 
-export default function Restaurant({ image, name, rates, reviews, altName }) {
+export default function Restaurant(props) {
+  const { Res_id, Res_img, Res_name, Rates, Reviews } = props;
   //여기는 componet가 구성되어있는 곳
+  console.log(props);
   return (
-    <div className="RestaurantContainer">
-      <img alt="{altName}" src="{image}" />
-      <h1>{name}</h1>
-      <span>{rates}</span> <span>{reviews}</span>
-    </div>
+    <>
+      <div className="List_Container">
+        <div className="List_Img">
+          <img key={Res_id} alt="Res_image" src={Res_img} />
+        </div>
+        <div className="List_Main">
+          <h3>{Res_name}</h3>
+          <div className="Res_info">
+            <span>{Rates}</span> <span>{Reviews}</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/pages/RestaurantList/RestaurantList.js
+++ src/pages/RestaurantList/RestaurantList.js
@@ -4,25 +4,26 @@ import './RestaurantList.scss';
 
 export default function RestaurantList() {
   // component를 구성해서 map을 돌린다.
-  const [restaurants, setStores] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/data/infoList.json')
       .then(res => res.json())
-      .then(result => setStores(result));
+      .then(result => setRestaurants(result));
   }, []);
 
   return (
     <div className="restaurantName">
       {restaurants.map(restaurant => {
         return (
-          <>
-            <Restaurant
-              key={restaurant.id}
-              id={restaurant.id}
-              name={restaurant.name}
-            />
-          </>
+          <Restaurant
+            key={restaurant.Res_id}
+            Res_img={restaurant.Res_img}
+            Res_name={restaurant.Res_name}
+            Rates={restaurant.Rates}
+            Reviews={restaurant.Reviews}
+            Reviews={restaurant.Reviews}
+          />
         );
       })}
     </div>
