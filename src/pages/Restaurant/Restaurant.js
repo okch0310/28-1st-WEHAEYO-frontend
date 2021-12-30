2655546617c12e45c6a2955b1fe0a5e31fd349ed
Young <81794430+Daeyeong-hub@users.noreply.github.com>
2021-12-30 21:52:07 +0900

    Modify: nesting 수정, mouse-cursor 기능 및 useNavigate 기능 등 추가

--- src/pages/Restaurant/Restaurant.js
+++ src/pages/Restaurant/Restaurant.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Restaurant.scss';
 
 export default function Restaurant({
@@ -10,10 +11,15 @@ export default function Restaurant({
   resAddress,
   resTelephone,
 }) {
-  //여기는 componet가 구성되어있는 곳
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/wehaeyo/src/pages/Main');
+    // 이후에 상세페이지로 넘어가는 것으로 수정 필요
+  };
+
   return (
     <>
-      <div className="Restaurant">
+      <div className="Restaurant" onClick={goToMain}>
         <div className="listImg">
           <img key={resId} alt="restaurantImage" src={resImg} />
         </div>
@@ -21,7 +27,7 @@ export default function Restaurant({
           <h3>{resName}</h3>
           <div className="resInfo">
             <span>별점: {resRates}</span>{' '}
-            <span className="reviewNumber">리뷰수: {resReviews}</span>
+            <span className="reviewNumber">리뷰갯수: {resReviews}</span>
             <p>주소: {resAddress}</p>
             <p>전화번호: {resTelephone}</p>
           </div>
