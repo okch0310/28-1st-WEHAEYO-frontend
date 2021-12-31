a8ab2725a37b790fa75de43a84836b2a00dca93a
minami-cs <mina9301@gmail.com>
2021-12-31 19:40:00 +0900

    Add: react-icons에서 가져온 별을 종류별로 각각 컴포넌트로 만들고 별점에 따라 표시되도록 하는 기능 추가

---
+++ src/pages/RestaurantDetail/FullStar.js
@@ -0,0 +1,5 @@
+import { ImStarFull } from 'react-icons/im';
+
+export default function FullStar() {
+  return <ImStarFull />;
+}
--- src/pages/RestaurantDetail/RestaurantInfo.js
+++ src/pages/RestaurantDetail/RestaurantInfo.js
@@ -1,7 +1,10 @@
-import { ImStarEmpty, ImStarHalf, ImStarFull } from 'react-icons/im';
 import { MdPlace } from 'react-icons/md';
 import { BsFillTelephoneFill } from 'react-icons/bs';
 import './RestaurantDetail.scss';
+import FullStar from './FullStar';
+import HalfStar from './HalfStar';
+import BlankStar from './BlankStar';
+import { useCallback } from 'react';
 
 export default function RestaurantInfo({
   title,
@@ -10,19 +13,32 @@ export default function RestaurantInfo({
   phone,
   rating,
 }) {
+  const renderStars = useCallback(() => {
+    const stringRating = rating.toString().split('.');
+    let resultArray = [];
+    for (let i = 0; i < Number(stringRating[0]); i++) {
+      resultArray.push(<FullStar key={i} />);
+    }
+    if (stringRating.length > 1)
+      resultArray.push(<HalfStar key={resultArray.length} />);
+    const resultArrayLength = resultArray.length;
+    if (Number(stringRating[0]) < 5) {
+      for (let j = 0; j < 5 - resultArrayLength; j++) {
+        resultArray.push(<BlankStar key={resultArrayLength + j} />);
+      }
+    }
+    return resultArray;
+  }, [rating]);
+
   return (
     <section className="restaurant_info">
       <img alt={`${title}`} src={`images/RestaurantDetail/${image}`} />
       <div className="info">
         <h1>{title}</h1>
-        <p className="stars">
-          <ImStarFull />
-          <ImStarHalf />
-          <ImStarEmpty />
-          <ImStarEmpty />
-          <ImStarEmpty />
-          {rating}점
-        </p>
+        <div className="stars">
+          {renderStars()}
+          &nbsp;{rating}점
+        </div>
         <p>리뷰 15개</p>
         <p>
           <MdPlace /> {address}
