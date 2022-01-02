ebe70c61b2f453a895208fa6b8352cb0e706eaa1
okch0310 <okch0310@naver.com>
2022-01-02 15:08:55 +0900

    [28기 옥채현] 임시

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -6,8 +6,8 @@ export default function Signup() {
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
   const [btn, setBtn] = useState(false);
-  const[name, setName]=useState('');
-  const [phone, setPhone]=useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
   //useState 추가
 
   function handleIdInput(e) {
@@ -18,20 +18,19 @@ export default function Signup() {
     setPw(e.target.value);
     btnActivate();
   }
-  
+
   function handlePhoneInput(e) {
-    setPw(e.target.value);
+    setPhone(e.target.value);
   }
 
   function handleNameInput(e) {
     setName(e.target.value);
   }
-  
+
   function btnActivate() {
     return id.includes('@') && pw.length > 1 ? setBtn(true) : setBtn(false);
   }
 
-  
   function requestSignUp() {
     fetch('https://2681-61-102-158-250.ngrok.io/users/signup', {
       method: 'POST',
@@ -41,10 +40,10 @@ export default function Signup() {
         email: id,
         password: pw,
       }),
-    }).then(response=>response.json());
+    })
+      .then(response => response.json())
       .then(result => console.log('결과: ', result));
   }
-  
 
   return (
     <div className="loginWrap">
@@ -55,13 +54,13 @@ export default function Signup() {
             className="inputBox"
             type="text"
             placeholder="이름"
-             onChange={e => handleNameInput(e)}
+            onChange={e => handleNameInput(e)}
           />
           <input
             className="inputBox"
             type="text"
             placeholder="전화번호 (ex:010-1234-5678)"
-             onChange={e => handlePhoneInput(e)}
+            onChange={e => handlePhoneInput(e)}
           />
           <input
             className="inputBox"
@@ -84,10 +83,7 @@ export default function Signup() {
               <Link to="/">아이디 찾기</Link>|<Link to="/">비밀번호 찾기</Link>
             </span>
           </div>
-          <button
-            className={btn ? 'btnOn' : 'btnOff'}
-            onClick={requestSignUp}
-          >
+          <button className={btn ? 'btnOn' : 'btnOff'} onClick={requestSignUp}>
             회원가입
           </button>
         </div>
