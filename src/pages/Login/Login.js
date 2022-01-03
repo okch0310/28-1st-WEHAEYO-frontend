745871489bafb5a2a2afb352ab98e5bdd6686c5c
okch0310 <okch0310@naver.com>
2022-01-03 13:42:02 +0900

    [28기 옥채현] Add: backend와 통신 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -18,18 +18,20 @@ export default function Login() {
   function btnActivate() {
     return id.includes('@') && pw.length > 1 ? setBtn(true) : setBtn(false);
   }
-  /*
+
   function requestLogin() {
-    fetch('http://10.58.3.111:8000/users/login', {
+    fetch('https://2681-61-102-158-250.ngrok.io/users/login', {
       method: 'POST',
       body: JSON.stringify({
         email: id,
         password: pw,
       }), //나중에 받을 값
-    }).then(response => response.json()); //우리가 자바스크립트가 읽을 수 있게 변경
-    // .then(result => console.log('결과: ', result)); //result 실제 데이터가 객체 형식으로 출력
-  }
-*/
+    })
+      .then(response => response.json()) //우리가 자바스크립트가 읽을 수 있게 변경
+      .then(result => {
+        localStorage.setItem('token', result.token);
+      });
+  } //result 실제 데이터가 객체 형식으로 출력
 
   return (
     <div className="login_wrap">
@@ -56,10 +58,7 @@ export default function Login() {
               <Link to="/">아이디 찾기</Link>|<Link to="/">비밀번호 찾기</Link>
             </span>
           </div>
-          <button
-            className={btn ? 'btnOn' : 'btnOff'}
-            //onClick={requestLogin}
-          >
+          <button className={btn ? 'btnOn' : 'btnOff'} onClick={requestLogin}>
             로그인
           </button>
         </div>
