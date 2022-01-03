37ef020ade9196822805cd6f86b2ac755f2819c6
minami-cs <mina9301@gmail.com>
2022-01-03 23:01:02 +0900

    Modify: 모달창에 css 적용 및 모달 내용수정

--- src/pages/RestaurantDetail/Modal.js
+++ src/pages/RestaurantDetail/Modal.js
@@ -9,27 +9,40 @@ export default function Modal() {
     <div>
       {isModalOpen && (
         <ModalLayout>
-          <div className="modal">
-            <div className="modal_content">
-              <div className="modal_menu">
+          <div className="modal_content">
+            <div className="modal_menu">
+              <div className="image_container">
                 <img
                   alt={modalContent?.menu_title}
                   src={`images/RestaurantDetail/${modalContent?.menu_image}`}
                 />
-                <p>{modalContent?.menu_title}</p>
               </div>
-              <div className="modal_options">
-                <ul>
-                  {modalContent?.options.map(option => {
-                    return (
-                      <li key={option.option_id}>
-                        <input type="radio" name="menu_option" />
-                        {option.content}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </div>
+              <div className="menu_title">{modalContent?.menu_title}</div>
+            </div>
+            <div className="modal_options">
+              <ul>
+                {modalContent?.options.map(option => {
+                  return (
+                    <li key={option.option_id}>
+                      <label htmlFor={'option' + option.option_id}>
+                        <div className="radio_option">
+                          <input
+                            type="radio"
+                            name="menu_option"
+                            id={'option' + option.option_id}
+                          />
+                          {option.content}
+                        </div>
+                        <div className="option_price">+ {option.price}원</div>
+                      </label>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className="menu_amount">
+              <p>수량</p>
+              <input type="number" min={1} />
             </div>
           </div>
         </ModalLayout>
--- src/pages/RestaurantDetail/ModalLayout.js
+++ src/pages/RestaurantDetail/ModalLayout.js
@@ -1,19 +1,23 @@
 import { useContext } from 'react';
 import { ModalContext } from './modalContext';
+import { GrClose } from 'react-icons/gr';
 
 export default function ModalLayout({ children }) {
   let { closeModal } = useContext(ModalContext);
   return (
-    <div className="modal_layout">
-      <div className="modal_header">
-        <p>메뉴상세</p>
-      </div>
-      {children}
-      <div className="modal_footer">
-        <button type="button" onClick={closeModal}>
-          취소하기
-        </button>
-        <button type="button">장바구니에 담기</button>
+    <div className="overlay">
+      <div className="modal_layout">
+        <div className="modal_header">
+          <p>메뉴상세</p>
+          <GrClose onClick={closeModal} />
+        </div>
+        {children}
+        <div className="modal_footer">
+          <button type="button" onClick={closeModal}>
+            취소하기
+          </button>
+          <button type="button">장바구니에 담기</button>
+        </div>
       </div>
     </div>
   );
