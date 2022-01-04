f9065c491c2fb1abad1740063963e46bdae0b0b3
okch0310 <okch0310@naver.com>
2022-01-04 21:58:51 +0900

    [28기 옥채현] Add: Main 페이지, backend와 연결 준비

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,1 +1,39 @@
-export default function Main() {}
+import React, { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
+import './Main.scss';
+
+export default function Main() {
+  const [foodKindList, setFoodKindList] = useState({});
+  useEffect(() => {
+    fetch('', { method: 'GET' }) //method get 은 생략 가능
+      .then(res => res.json())
+      .then(res => setFoodKindList(res));
+  }, []);
+  return (
+    <div className="Main">
+      <div className="mainPage1">
+        <img
+          className="mainImg"
+          src="https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          alt="메인이미지"
+        />
+        <div className="mainSearchWrap">
+          <input className="mainInput" placeholder="검색" />
+          <button className="mainBtn">검색</button>
+        </div>
+      </div>
+      <div className="mainPage2">
+        <div className="foodKindList">
+          {foodKindList.map((ele, idx) => {
+            return (
+              <div className="foodKind" key={idx}>
+                <div>{ele.category_name}</div>
+                <img src={ele.img_url} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
