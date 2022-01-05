eb4ffb18bdc5ac01f1ac6992ab85becef9701bc3
minami-cs <mina9301@gmail.com>
2022-01-05 19:32:37 +0900

    Add: 장바구니 담기 확인 토스트 메시지 기능 추가

--- src/pages/RestaurantDetail/Modal/ModalLayout.js
+++ src/pages/RestaurantDetail/Modal/ModalLayout.js
@@ -1,10 +1,23 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ModalContext } from '../modalContext';
 import { GrClose } from 'react-icons/gr';
+import Toast from '../Toast/Toast';
 import './Modal.scss';
 
 export default function ModalLayout({ children }) {
   let { closeModal } = useContext(ModalContext);
+  const [isActiveToast, setIsActiveToast] = useState(false);
+
+  const submitMenuOrder = () => {
+    setIsActiveToast(true);
+  };
+  useEffect(() => {
+    if (isActiveToast) {
+      setTimeout(() => {
+        setIsActiveToast(false);
+      }, 2000);
+    }
+  }, [isActiveToast]);
 
   return (
     <>
@@ -21,9 +34,12 @@ export default function ModalLayout({ children }) {
           <button type="button" onClick={closeModal}>
             취소하기
           </button>
-          <button type="button">장바구니에 담기</button>
+          <button type="button" onClick={submitMenuOrder}>
+            장바구니에 담기
+          </button>
         </div>
       </div>
+      {isActiveToast && <Toast />}
     </>
   );
 }
