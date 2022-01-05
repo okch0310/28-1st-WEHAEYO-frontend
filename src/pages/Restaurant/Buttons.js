95deb2ed17abef7cb777135c7a8708f94fcf2c67
Young <81794430+Daeyeong-hub@users.noreply.github.com>
2022-01-05 09:30:20 +0900

    Add: 쿼리스트링 구현 중(임의 키값등으로 구현 중, 백과 맞춰서 수정필요

--- src/pages/Restaurant/Buttons.js
+++ src/pages/Restaurant/Buttons.js
@@ -3,8 +3,9 @@ import './Buttons.scss';
 
 export default function Buttons({ updateKeywords }) {
   return (
+    //클릭을 헀을 떄 all, korean, chinese, japanese 등의 키워드를 매개인자로 updateKeywords 함수에 보낸다.
     <div className="restaurantMenuTap">
-      <button onClick={() => updateKeywords('All')} value="all">
+      <button onClick={() => updateKeywords('all')} value="all">
         전체보기
       </button>
       <button onClick={() => updateKeywords('korean')} value="korean">
--- src/pages/RestaurantList/RestaurantList.js
+++ src/pages/RestaurantList/RestaurantList.js
@@ -10,7 +10,7 @@ export default function RestaurantList() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  console.log('location', location);  
+  console.log('location', location);
 
   //데이터 로딩
   useEffect(() => {
@@ -19,18 +19,20 @@ export default function RestaurantList() {
       .then(result => setRestaurants(result));
   }, [location.search]);
 
-  const updateKeywords = (buttonKeyword) => {
+  const updateKeywords = buttonKeyword => {
+    //button에서 전달이 된 매개인자를 keyword 변수에 담는다.
     const keyword = 'buttonKeyword';
-    const queryString =`keyword=${keyword}`; 
+    //변수르 활용해서 쿼리스트링 형식으로 만든다.
+    const queryString = `?keyword=${keyword}`;
     console.log(keyword);
     console.log(queryString);
-
+    //쿼리스트링 url로 이동한다
     navigate(`/restaurants?keyword=${queryString}`);
   };
 
-  const updateCategories = (optionKeyword) => {
+  const updateCategories = optionKeyword => {
     const sortedOption = 'optionKeyword';
-    const sortedqueryString =`sortedOption=${sortedOption}`; 
+    const sortedqueryString = `sortedOption=${sortedOption}`;
     console.log(sortedOption);
     console.log(sortedqueryString);
 
@@ -39,20 +41,20 @@ export default function RestaurantList() {
 
   return (
     <div className="restaurantList">
-    
-
       <div className="restaurantListMain">
         {restaurants.map(restaurant => {
           return (
-            <SelectBar updateCategories={updateCategories}/>
-            <Buttons updateKeywords={updateKeywords}/>
-            <Restaurant
-              key={restaurant.id}
-              img={restaurant.img}
-              name={restaurant.name}
-              rates={restaurant.rates}
-              reviews={restaurant.reviews}
-            />
+            <>
+              <SelectBar updateCategories={updateCategories} />
+              <Buttons updateKeywords={updateKeywords} />
+              <Restaurant
+                key={restaurant.id}
+                img={restaurant.img}
+                name={restaurant.name}
+                rates={restaurant.rates}
+                reviews={restaurant.reviews}
+              />
+            </>
           );
         })}
       </div>
