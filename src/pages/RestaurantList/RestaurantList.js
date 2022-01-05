5f28cc1185f1b04175f3e8936a2622180bba6ffa
Young <81794430+Daeyeong-hub@users.noreply.github.com>
2022-01-05 14:59:41 +0900

    modify: 여러가지 시도 중

--- src/pages/RestaurantList/RestaurantList.js
+++ src/pages/RestaurantList/RestaurantList.js
@@ -5,6 +5,9 @@ import './RestaurantList.scss';
 import Buttons from '../Restaurant/Buttons';
 import SelectBar from '../Restaurant/SelectBar';
 
+// 값이 있을 떄만 url에 keyword, selectedOption을 넣어주는 형식
+//객체로 받아오던
+
 export default function RestaurantList() {
   const [restaurants, setRestaurants] = useState([]);
   const navigate = useNavigate();
@@ -12,6 +15,10 @@ export default function RestaurantList() {
   //현재 있는 쿼리파라미터의 정보가 나온다(useLocation이 반환하는 값 중에서 search에 담김)
 
   console.log('location', location);
+
+  const [selectedKeyword, setKeyword] = useState('');
+  //키워드 관련 쿼리스트링이 selectedKeyword 담겨있다.
+  console.log(selectedKeyword);
 
   //데이터 로딩
   //쿼리스트링 주소가 담김 url을 받아오게 된다(`{API}/user${location.search}`)
@@ -24,6 +31,8 @@ export default function RestaurantList() {
   }, [location.search]);
 
   console.log(restaurants);
+
+  //버튼 클릭시 카테고리 별로 보여주기
   const updateKeywords = buttonKeyword => {
     //button에서 전달이 된 매개인자를 keyword 변수에 담는다. korean, japanese, chinese
     const keyword = buttonKeyword;
@@ -33,17 +42,17 @@ export default function RestaurantList() {
     console.log(queryString);
     //쿼리스트링 url로 이동한다
     navigate(`/restaurants/list${queryString}`);
+    setKeyword(queryString);
   };
 
+  //별점과 리뷰 순대로 정렬하기 기능
   const updateCategories = optionKeyword => {
     const sortedOption = optionKeyword;
     const sortedqueryString = `sort=${sortedOption}`;
     console.log(sortedOption);
     console.log(sortedqueryString);
 
-    navigate(
-      `/restaurants/list?category=${sortedqueryString}&${sortedqueryString}`
-    );
+    navigate(`/restaurants/list${selectedKeyword}&${sortedqueryString}`);
   };
 
   return (
