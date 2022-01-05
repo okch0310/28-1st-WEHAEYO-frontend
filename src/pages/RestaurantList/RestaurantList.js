d72e7e00d07d001f55e5b8b2b74b6188fc6e8936
Young <81794430+Daeyeong-hub@users.noreply.github.com>
2022-01-05 09:35:26 +0900

    Add: 쿼리스트링 구현 중이지만 많은 부분 수정필요

--- src/pages/RestaurantList/RestaurantList.js
+++ src/pages/RestaurantList/RestaurantList.js
@@ -9,10 +9,12 @@ export default function RestaurantList() {
   const [restaurants, setRestaurants] = useState([]);
   const navigate = useNavigate();
   const location = useLocation();
+  //현재 있는 쿼리파라미터의 정보가 나온다(useLocation이 반환하는 값 중에서 search에 담김)
 
   console.log('location', location);
 
   //데이터 로딩
+  //쿼리스트링 주소가 담김 url을 받아오게 된다(`{API}/user${location.search}`)
   useEffect(() => {
     fetch(`{API}/user${location.search}`)
       .then(res => res.json())
@@ -22,7 +24,7 @@ export default function RestaurantList() {
   const updateKeywords = buttonKeyword => {
     //button에서 전달이 된 매개인자를 keyword 변수에 담는다.
     const keyword = 'buttonKeyword';
-    //변수르 활용해서 쿼리스트링 형식으로 만든다.
+    //변수를 활용해서 쿼리스트링 형식으로 만든다.
     const queryString = `?keyword=${keyword}`;
     console.log(keyword);
     console.log(queryString);
